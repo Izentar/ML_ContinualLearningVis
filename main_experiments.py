7c9d0d2a72a9243bfcd2300a83b88c086c8bc121
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-02-12 11:40:37 +0100

    added experiments

--- main_experiments.py
+++ main_experiments.py
@@ -6,9 +6,12 @@ import shlex
 import traceback
 from datetime import datetime
 
-def main():
+def main(fast_dev_run=True):
+    """
+        Change fast_dev_run flag to run full experiments.
+    """
     if __name__ == "__main__":
-        REPEAT = 1
+        REPEAT = 3
         
         parser = my_parser.main_arg_parser()
         parser = my_parser.layer_statistics_arg_parser(parser)
@@ -16,12 +19,19 @@ def main():
         parser = my_parser.model_statistics_optim_arg_parser(parser)
 
         time = datetime.today().strftime('%Y-%m-%d=%H-%M-%S')
-        
+
+        print(f"Experiments to be run:")
+        for v in experiments.values():
+            print(f"\t* v")
+
+        print()
         for k, v in experiments.items():
             for idx in range(1, REPEAT + 1):
                 print(f"Running experiment: {k}; repeat {idx}/{REPEAT}")
 
                 v = v.replace('\n', ' ')
+                if(fast_dev_run):
+                    v += ' -f'
                 args_exp = my_parser.parse_args(parser, shlex.split(v))
                 try:
                     logic(args_exp, True, project_name=f"exp_{time}", run_name=k)
@@ -37,41 +47,61 @@ def main():
                 print("Done")
                 
 
+crossentropy_default_c10_sgd = """
+-d c10 --model.num_classes 10 --loop.schedule 200 \
+--config.framework_type crossentropy-default --model.type \
+custom-resnet34 --loop.num_loops 1 --loop.train_at 0 \
+--model.optim.type sgd --model.optim.kwargs.lr 0.1 \
+--model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1 \
+--model.sched.kwargs.milestones 100 150 --datamodule.num_workers 3 \
+--loop.save.root model_save/test --loop.save.model \
+"""
+
+crossentropy_default_c100_sgd = """
+-d c100 --model.num_classes 100 --loop.schedule 200 \
+--config.framework_type crossentropy-default --model.type \
+custom-resnet34 --loop.num_loops 1 --loop.train_at 0 \
+--model.optim.type sgd --model.optim.kwargs.lr 0.1 \
+--model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1 \
+--model.sched.kwargs.milestones 100 150 --datamodule.num_workers 3 \
+--loop.save.root model_save/test --loop.save.model \
+"""
+
+chi_sqr_c10_sgd = """
+-d c10 --model.num_classes 10 --loop.schedule 260 \
+--config.framework_type latent-multitarget --model.type \
+custom-resnet34 --loop.num_loops 1 --loop.train_at 0 \
+--model.optim.type sgd --model.optim.kwargs.lr 0.1 \
+--model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1 \
+--model.sched.kwargs.milestones 140 180 --datamodule.num_workers 3 \
+--loop.save.root model_save/test --loop.save.model --loop.load.root \
+model_save/test --model.latent.size 3 --stat.collect_stats.enable \
+--model.loss.chi.shift_min_distance 0 --model.loss.chi.ratio 10 \
+--model.loss.chi.scale 15 --model.loss.chi.ratio_gamma 2 \
+--model.loss.chi.ratio_milestones 5 20 40 60 \
+"""
+
+chi_sqr_c100_sgd = """
+-d c100 --model.num_classes 100 --loop.schedule 260 \
+--config.framework_type latent-multitarget --model.type \
+custom-resnet34 --loop.num_loops 1 --loop.train_at 0 \
+--model.optim.type sgd --model.optim.kwargs.lr 0.1 \
+--model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1 \
+--model.sched.kwargs.milestones 140 180 --datamodule.num_workers 3 \
+--loop.save.root model_save/test --loop.save.model --loop.load.root \
+model_save/test --model.latent.size 30 --stat.collect_stats.enable \
+--model.loss.chi.shift_min_distance 0 --model.loss.chi.ratio 5 \
+--model.loss.chi.scale 40 --model.loss.chi.ratio_gamma 2 \
+--model.loss.chi.ratio_milestones 5 20 40 60 \
+""",
+
 experiments = {
-    "exp_1": """
--d c10 --model.num_classes 10 --loop.schedule 200
---config.framework_type crossentropy-default --model.type
-custom-resnet34 --loop.num_loops 1 --loop.train_at 0
---model.optim.type sgd --model.optim.kwargs.lr 0.1
---model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1
---model.sched.kwargs.milestones 100 150 --datamodule.num_workers 3
---loop.save.root model_save/test --loop.save.model -f
-""",
-
-    "exp_2":  """
--d c10 --model.num_classes 10 --loop.schedule 260
---config.framework_type latent-multitarget --model.type
-custom-resnet34 --loop.num_loops 1 --loop.train_at 0
---model.optim.type sgd --model.optim.kwargs.lr 0.1
---model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1
---model.sched.kwargs.milestones 140 180 --datamodule.num_workers 3
---loop.save.root model_save/test --loop.save.model --loop.load.root
-model_save/test --model.latent.size 3 --stat.collect_stats.enable
---model.loss.chi.shift_min_distance 0 --model.loss.chi.ratio 10
---model.loss.chi.scale 10 --model.loss.chi.ratio_gamma 2
---model.loss.chi.ratio_milestones 5 20 40 60 -f
-""",
-
-"exp_3": """
--d c10 --model.num_classes 10 --loop.schedule 200
---config.framework_type crossentropy-default --model.type
-custom-resnet34 --loop.num_loops 1 --loop.train_at 0
---model.optim.type sgd --model.optim.kwargs.lr 0.1
---model.sched.type MULTISTEP-SCHED --model.sched.kwargs.gamma 0.1
---model.sched.kwargs.milestones 100 150 --datamodule.num_workers 3
---loop.save.root model_save/test --loop.save.model -f
-""",
+    "crossentropy_default_c10_sgd": crossentropy_default_c10_sgd,
+    "crossentropy_default_c100_sgd": crossentropy_default_c10_sgd,
+    "chi_sqr_c10_sgd": chi_sqr_c10_sgd,
+    "chi_sqr_c100_sgd": chi_sqr_c100_sgd,
 }
+
 
 
 
