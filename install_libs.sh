90c308b8bb603f29ba25296942605e82f1670851
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-02-14 11:24:32 +0100

    force python3

--- install_libs.sh
+++ install_libs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-python -m venv pythonEnv
+python3 -m venv pythonEnv
 source pythonEnv/bin/activate
 
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
-pip install pytorch-lightning==1.6.4 torch-lucent cox==0.1.post3 pandas dill==0.3.5.1 tensorboardX==2.5.1 wandb pytorchcv==0.0.67 rich==12.4.4 matplotlib colorama
-pip install --upgrade requests
-pip install wandb
+python3 -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+python3 -m pip install pytorch-lightning==1.6.4 torch-lucent cox==0.1.post3 pandas dill==0.3.5.1 tensorboardX==2.5.1 wandb pytorchcv==0.0.67 rich==12.4.4 matplotlib colorama
+python3 -m pip install --upgrade requests
+python3 -m pip install wandb
