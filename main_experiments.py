9affa574da3519bb3ac6289a6d1aee04b13d61bb
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-02-14 11:51:57 +0100

    fix last experiment run

--- main_experiments.py
+++ main_experiments.py
@@ -90,8 +90,8 @@ custom-resnet34 --loop.num_loops 1 --loop.train_at 0 \
 --model.sched.kwargs.milestones 140 180 --datamodule.num_workers 3 \
 --loop.save.root model_save/test --loop.save.model --loop.load.root \
 model_save/test --model.latent.size 30 --stat.collect_stats.enable \
---model.loss.chi.shift_min_distance 0 --model.loss.chi.ratio 5 \
---model.loss.chi.scale 40 --model.loss.chi.ratio_gamma 2 \
+--model.loss.chi.shift_min_distance 0 --model.loss.chi.ratio 10 \
+--model.loss.chi.scale 60 --model.loss.chi.ratio_gamma 2 \
 --model.loss.chi.ratio_milestones 5 20 40 60 \
 """
 
