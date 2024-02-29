5cd3e045b2e341e3112ce558906426e5f52de89f
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-02-29 20:51:02 +0100

    update

--- update_system.sh
+++ update_system.sh
@@ -4,4 +4,5 @@ set -e
 sudo apt update
 sudo apt install -y python3.11 python3-pip python3.11-venv 
 sudo apt install -y ubuntu-drivers-common
+sudo apt install -y alsa-utils
 sudo ubuntu-drivers install
