8f038c52c6219c4b4447cac01db26e15566ed8e0
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-03-01 22:06:36 +0100

    new update-alternatives python

--- update_system.sh
+++ update_system.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+# use on your own risk !!!
+
 sudo apt update
 sudo apt install -y python3.11 python3-pip python3.11-venv 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
 sudo apt install -y ubuntu-drivers-common
 sudo apt install -y alsa-utils
 sudo ubuntu-drivers install
