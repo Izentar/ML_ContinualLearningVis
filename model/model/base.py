3cf6cc7aff43e8751110b154f0d79dfb32a52a7c
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-04-09 00:47:37 +0200

    uniform reset_parameters across all invocations

--- model/model/base.py
+++ model/model/base.py
@@ -5,18 +5,7 @@ from torch import nn
 class ModelBase():
     def _initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+            m.reset_parameters()
 
     @abstractmethod
     def forward(self, x, **kwargs):
