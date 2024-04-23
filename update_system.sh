82f6b96bb132bc24bcc39ed3c21aa637440d1d0f
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-04-23 23:07:39 +0200

    update

--- update_system.sh
+++ update_system.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-# do not invoke this script
-# use only needed commands 
-# use on your own risk !!!
+echo "do not invoke this script"
+echo "use only needed commands"
+echo "use on your own risk !!!"
+
+exit 1
+
 
 sudo apt update
-sudo apt install -y python3.10 python3-pip python3.10-venv 
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+sudo apt install -y python3 python3-pip python3-venv 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3 2
 sudo apt install -y ubuntu-drivers-common
 sudo apt install -y alsa-utils
 sudo ubuntu-drivers install
