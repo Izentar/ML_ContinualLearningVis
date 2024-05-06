2237efe72af73b25d192920a31dce36a456faa02
Mateusz Kordowski <mateuszkd67@gmail.com>
2024-05-06 10:26:50 +0200

    remove unused files

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ import unittest
 #import tests.dataset.test_CLModule
 
 if __name__ == '__main__':
-    unittest.main()
+    print("Unit tests not implemented. Testing using the black box idea. To use it, run main.py with flags that starts with '--stat.' and use any additional other arguments at your wish.")
+    #unittest.main()
